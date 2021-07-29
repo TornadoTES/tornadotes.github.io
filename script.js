cfab76a6c34365443808f0fe5fbabf9cf6011641
TES <80611951+TornadoTES@users.noreply.github.com>
2021-07-29 10:05:49 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function menu() {
-            location.replace("menu.html");
-}
-function trouble() {
-            location.replace("troublemakers.html");
-}
-function home() {
-            location.replace("index.html");
-}
