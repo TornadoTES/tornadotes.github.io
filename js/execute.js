216da743587414855fd5d9fdf33d8a16170bc6e3
TES <80611951+TornadoTES@users.noreply.github.com>
2021-07-29 10:09:56 -0500

    Update execute.js

--- js/execute.js
+++ js/execute.js
@@ -1,12 +1,12 @@
 function menu() {
-            location.replace("https://tornadotes.github.io/menu.html");
+            window.replace("https://tornadotes.github.io/menu.html");
 }
 function trouble() {
-            location.replace("https://tornadotes.github.io/troublemakers.html");
+            window.replace("https://tornadotes.github.io/troublemakers.html");
 }
 function home() {
-            location.replace("https://tornadotes.github.io/index.html");
+            window.replace("https://tornadotes.github.io/index.html");
 }
 function staff() {
-            location.replace("https://tornadotes.github.io/staffroll.html");
+            window.replace("https://tornadotes.github.io/staffroll.html");
 }
