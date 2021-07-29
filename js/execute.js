79069cb03f04ddf77235bc36060161cdcc0e32c7
TES <80611951+TornadoTES@users.noreply.github.com>
2021-07-29 09:38:31 -0500

    Add files via upload

---
+++ js/execute.js
@@ -0,0 +1,12 @@
+function menu() {
+            location.replace("../wtb-main/menu.html");
+}
+function trouble() {
+            location.replace("../wtb-main/troublemakers.html");
+}
+function home() {
+            location.replace("../wtb-main/index.html");
+}
+function staff() {
+            location.replace("../wtb-main/staffroll.html");
+}
---
+++ script.js
@@ -0,0 +1,9 @@
+function menu() {
+            location.replace("menu.html");
+}
+function trouble() {
+            location.replace("troublemakers.html");
+}
+function home() {
+            location.replace("index.html");
+}
