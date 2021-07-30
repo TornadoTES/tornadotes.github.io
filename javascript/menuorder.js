cb0f1c94d3ccc29f8e8cc828a3718b0cbf7ac287
TES <80611951+TornadoTES@users.noreply.github.com>
2021-07-30 09:43:34 -0500

    Update menuorder.js

--- javascript/menuorder.js
+++ javascript/menuorder.js
@@ -2,6 +2,6 @@ function getInputValue(){
             var inputVal = document.getElementById("order").value;
             if(inputVal = "Classic Dark Pump" || "Pump" || "pump" || "Pump Bowl" || "Bowl" || "bowl" || "Seed Plate" || "Seed" || "seed" || "Brioche" || "brioche" || "Walnuts" || "nuts" || "walnuts" || "Corn" || "corn" || "Carrots" || "carrots" || "Stuffed Mini Pump" || "stuffed pump" || "Apple Salad" || "salad" || "apple salad"){
                 window.open("https://tornadotes.github.io/tracker.html");
-                else{
+            } else {
                     messagetext("Sorry, please try again.");
 }
