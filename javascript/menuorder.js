10ed44b236e12fef9b24fb56e45c12f33bf9744e
TES <80611951+TornadoTES@users.noreply.github.com>
2021-07-30 09:52:57 -0500

    Update menuorder.js

--- javascript/menuorder.js
+++ javascript/menuorder.js
@@ -1,7 +1,1 @@
-function getInputValue(){
-            var inputVal = document.getElementById("order").value;
-            if(inputVal = "Classic Dark Pump" || "Pump" || "pump" || "Pump Bowl" || "Bowl" || "bowl" || "Seed Plate" || "Seed" || "seed" || "Brioche" || "brioche" || "Walnuts" || "nuts" || "walnuts" || "Corn" || "corn" || "Carrots" || "carrots" || "Stuffed Mini Pump" || "stuffed pump" || "Apple Salad" || "salad" || "apple salad"){
-                window.open("https://tornadotes.github.io/tracker.html");
-            } else {
-                    messagetext("Sorry, please try again.");
-}
+
