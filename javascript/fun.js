aaab93670d64f649b86e640468fc5a4d9d578554
TES <80611951+TornadoTES@users.noreply.github.com>
2021-07-30 10:13:11 -0500

    Update fun.js

--- javascript/fun.js
+++ javascript/fun.js
@@ -18,10 +18,10 @@ function order() {
             window.close();
             window.open("https://tornadotes.github.io/order.html");
 }
-function getInputValue(){
+function getInputValue() {
             var inputVal = document.getElementById("order").value;
             if(inputVal = "Classic Dark Pump" || "Pump" || "pump" || "Pump Bowl" || "Bowl" || "bowl" || "Seed Plate" || "Seed" || "seed" || "Brioche" || "brioche" || "Walnuts" || "nuts" || "walnuts" || "Corn" || "corn" || "Carrots" || "carrots" || "Stuffed Mini Pump" || "stuffed pump" || "Apple Salad" || "salad" || "apple salad"){
                 window.open("https://tornadotes.github.io/tracker.html");
             } else {
                     messagetext("Sorry, please try again.");
-})
+}
